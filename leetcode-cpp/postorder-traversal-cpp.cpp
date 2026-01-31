f47ce010445c35da22fdf54ad232de1098c02002
John Desmond <johndesmond631@gmail.com>
2026-01-31 14:00:41 -0500

    postorder

---
+++ leetcode-cpp/postorder-traversal-cpp.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void helper(TreeNode* root, vector<int>& output) {
+        if (root == nullptr) return;
+        
+        helper(root->left, output);
+        helper(root->right, output);
+        output.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> output;
+        helper(root, output);
+        return output;
+    }
+};
